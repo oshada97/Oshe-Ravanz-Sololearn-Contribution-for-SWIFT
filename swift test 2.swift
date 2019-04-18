811ebc27ed3e7804dade4cde6c618d98c7915732
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-18 23:59:09 +0530

    assign variable and print its value

---
+++ swift test 2.swift
@@ -0,0 +1,12 @@
+//oshe ravanz
+//oshada97
+//mr.basnayake
+//Oshada Basnayake
+//ceberus labs
+//oshe ceylon
+
+//further contribution for the world will be shown with above names
+
+var osheData = 34  //calling variable
+
+print(osheData) //print variable value
